6929432671cb31ca1ac7fb48393106ffe6c21ab5
Raul Iglesias <raul@push-founders.com>
2020-04-22 23:32:26 +0200

    Features/vuex store (#3)
    * Added store, actions and mutations

---
+++ src/views/Pet.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ animal.name }} ({{ $route.params.species }})</h1>
+    <div>Age: {{ animal.age }} years old</div>
+    <div>Breed: {{ animal.breed }}</div>
+  </div>
+</template>
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  data() {
+    return {
+      animal: {}
+    };
+  },
+  computed: {
+    ...mapState(['cats', 'dogs'])
+  },
+  mounted() {
+    this.animal = this[this.$route.params.species][this.$route.params.id];
+  }
+};
+</script>
